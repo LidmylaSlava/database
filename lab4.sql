c48262678c2d32eaac7ff6514eafe0769d65c23b
LidmylaSlava <liudmyla.slava@gmail.com>
2016-11-11 02:49:23 +0200

    Update lab4.sql

--- lab4.sql
+++ lab4.sql
@@ -2,3 +2,15 @@
 - Вибрати назви або номери приміщень за підрозділами;
 - Підрахувати загальну площу навчальних аудиторій по приміщеннях і в цілому по навчальному закладу;
 - Підрахувати загальну кількість посадочних місць для співробітників по підрозділам.*/
+SELECT rooms.room_number NUMBER, subdivisions.title
+FROM rooms, subdivisions
+WHERE rooms.subdivision_id = subdivisions.id;
+
+SELECT room_type, SUM(square) FROM rooms
+GROUP BY room_type;
+
+SELECT SUM(square) FROM rooms;
+
+SELECT subdivisions.title, count(capacity)
+FROM subdivisions, rooms
+GROUP BY subdivisions.title;
