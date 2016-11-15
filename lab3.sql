f8e5048b99770a6d2396a0e0317eb4261bec3bc3
LidmylaSlava <liudmyla.slava@gmail.com>
2016-11-15 03:39:11 +0200

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -1,16 +1,48 @@
-INSERT INTO subdivisions(title, type_sub) VALUES
-    ('FF', 'administrative'),
-    ('IB', 'office'),
-    ('FF', 'office');
+INSERT INTO rooms_type(room_type) VALUES
+    ('lecture hall'),
+    ('lecture hall'),
+    ('cabinet'),
+    ('laboratory'),
+    ('laboratory'),
+    ('cabinet'),
+    ('cabinet'),
+    ('lecture hall'),
+    ('lecture hall'),
+    ('laboratory');
 
-INSERT INTO rooms(room_type, subdivision_id, room_number, square, capacity) VALUES
-    ('lecture hall', 1, 1, 150, 100),
-    ('lecture hall', 1, 2, 200, 50),
-    ('cabinet', 2, 30, 20, 2),
-    ('laboratory', 3, 32, 50, 30),
-    ('laboratory', 4, 32, 60, 20),
-    ('cabinet', 3, 40, 62, 10),
-    ('cabinet', 2, 31, 70, 5),
-    ('lecture hall', 1, 102, 120, 70),
-    ('lecture hall', 1, 105, 300, 200),
-    ('laboratory', 4, 125, 100, 60);
+INSERT INTO rooms(room_type_id, subdivision_id, room_number, square, capacity) VALUES
+  (1, 1, 108, 100, 90),
+  (2, 2, 312, 60, 25),
+  (3, 3, 114, 50, 10),
+  (4, 4, 118, 100, 80),
+  (5, 5, 12, 30, 5),
+  (6, 6, 303, 50, 35),
+  (7, 7, 112, 48, 40);
+  
+INSERT INTO type_subdivisions (type_sub) VALUES
+('training room'),
+('office'),
+('administrative'),
+('training room'),
+('office'),
+('administrative'),
+('training room'),
+('office'),
+('administrative');
+
+
+INSERT INTO subdivisions(title, type_sub) VALUES
+    ('IBdepartment', 1),
+    ('APdepartment', 2),
+    ('сryptodepartment', 3),
+    ('deanery', 4),
+    ('deanery', 5),
+     ('IBdepartment', 6),
+    ('APdepartment', 7),
+    ('сryptodepartment', 8),
+    ('deanery', 9),
+    ('deanery', 10);
+    ('IBdepartment', 11),
+    ('сryptodepartment', 12);
+
+   
