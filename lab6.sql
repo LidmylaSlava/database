3881fcb0eb455d61b61088c23b7f4493c0363100
LidmylaSlava <liudmyla.slava@gmail.com>
2016-11-25 07:50:39 +0200

    Update lab6.sql

--- lab6.sql
+++ lab6.sql
@@ -1,1 +1,15 @@
 
+/*2. Створити представлення «Аудиторії» з полями «код_приміщення», «назва приміщення», «підрозділ», 
+в яку повинні входити приміщення виду «Аудиторія». Оновлювати представлення «Аудиторії».*/
+CREATE VIEW LECTUREHALL
+AS
+SELECT r.id, r.room_number,
+s.title
+FROM rooms r INNER JOIN subdivisions s
+ON r.subdivision_id=s.id
+INNER JOIN rooms_type rt 
+ON r.room_type_id=rt.id
+WHERE room_type='lecture hall';
+
+
+
