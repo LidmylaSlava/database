7b81dcec5d1802af3d05192440991bae2031b8e9
LidmylaSlava <liudmyla.slava@gmail.com>
2016-11-25 04:24:05 +0200

    Update lab4.sql

--- lab4.sql
+++ lab4.sql
@@ -6,11 +6,25 @@ SELECT rooms.room_number NUMBER, subdivisions.title
 FROM rooms, subdivisions
 WHERE rooms.subdivision_id = subdivisions.id;
 
-SELECT room_type, SUM(square) FROM rooms
-GROUP BY room_type;
+SELECT title, SUM(square) -- SUM square by subdivisions
+FROM subdivisions S INNER JOIN type_subdivisions TS
+ON S.type_sub_id=TS.id
+INNER JOIN rooms R
+ON S.id=R.subdivision_id
+INNER JOIN rooms_type RT
+ON R.room_type_id=RT.id
+GROUP BY title;
 
 SELECT SUM(square) FROM rooms;
 
 SELECT subdivisions.title, SUM(capacity)
 FROM subdivisions, rooms
 GROUP BY subdivisions.title;
+
+SELECT NameGroup, Round(AVG(Mark),1)[Средняя оценка]
+FROM Progress P INNER JOIN Student S
+ON P.NRecordBook=S.NRecordBook
+INNER JOIN SGroup SG
+ON S.IDGroup=SG.IDGroup
+GROUP BY NameGroup
+HAVING AVG(Mark)>3
