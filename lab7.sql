acf9148995fa8936ed2dd5c2c3262d3d0c8ec346
LidmylaSlava <liudmyla.slava@gmail.com>
2016-11-28 22:39:11 +0200

    Update lab7.sql

--- lab7.sql
+++ lab7.sql
@@ -1,1 +1,11 @@
-
+-- Процедура повинна повертати кількість приміщень для зазначеного підрозділу.
+CREATE OR REPLACE FUNCTION get_rooms_count_by_subdivision (stitle varchar(225)) RETURNS INTEGER AS $$
+DECLARE
+  ret INTEGER;
+  BEGIN
+  SELECT INTO ret COUNT(r.room_number) FROM rooms r INNER JOIN subdivisions s
+  ON r.subdivision_id=s.id WHERE s.title=title;
+  RETURN ret;
+  END;
+  $$ language plpgsql;
+  SELECT get_rooms_count_by_subdivision ('deanery');
