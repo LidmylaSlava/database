c4001cf1f7fe6d8530551f7482eafb4b1340cc21
LidmylaSlava <liudmyla.slava@gmail.com>
2016-11-28 23:01:51 +0200

    Create lab9.sql

---
+++ lab9.sql
@@ -0,0 +1,20 @@
+/* 1. Напишіть команди Transact SQL для наступних дій:
+1. Створити новий обліковий запис;
+2. Назначити користувачу роль БД (будь-яку);
+3. Надати користувачу Bill повноваження на доступ к збереженої
+процедури (будь-якої);
+4. Відкликати всі надані користувачу John привілегії.
+2. Поясніть призначення наступних команд Transact SQL:
+a) GRANT SELECT, INSERT
+ON SUPPLIES
+TO J_Smith
+WITH GRANT OPTION AS Economists
+b) EXEC[UTE] sp_addlogin ‘king_of_the_db',
+‘a2h7d0f7dg84mdf94',
+‘PROJECTS',
+‘Ukrainian',
+‘master',
+‘NULL'
+c) REVOKE ALL TO ‘M_Ivanenko'
+CASCADE
+3. Зашифруйте зміст однієї з колонок таблиці в створеній БД.*/
