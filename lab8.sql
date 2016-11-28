3719f7f4ca244851adaf0e47f3c2306409c3fdd9
LidmylaSlava <liudmyla.slava@gmail.com>
2016-11-29 00:46:01 +0200

    Update lab8.sql

--- lab8.sql
+++ lab8.sql
@@ -3,4 +3,19 @@
 Якщо для приміщення зазначено підрозділ, заборонити видалення запису.
 2. Створити представлення «Аудиторії» з полями «код_приміщення», «назва приміщення», 
 «підрозділ», в яку повинні входити приміщення виду «Аудиторія». Оновлювати представлення «Аудиторії».*/
+CREATE TRIGGER TR_ROOM_DEL ON rooms
+INSTEAD OF DELETE 
+AS
+BEGIN
+ SET NOCOUNT ON;
+ DELETE r
+ FROM rooms r
+ INNER JOIN deleted AS d
+ ON r.id=d.id 
+ INNER JOIN subdivisions s
+ ON r.subdivision_id=s.id
+ WHERE s.title=NOT NULL;
+ END;
+
+
 
