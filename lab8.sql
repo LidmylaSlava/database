d97b3097896765a9009ad16a70841e6bde0fba44
LidmylaSlava <liudmyla.slava@gmail.com>
2016-11-29 02:06:34 +0200

    Update lab8.sql

--- lab8.sql
+++ lab8.sql
@@ -3,19 +3,21 @@
 Якщо для приміщення зазначено підрозділ, заборонити видалення запису.
 2. Створити представлення «Аудиторії» з полями «код_приміщення», «назва приміщення», 
 «підрозділ», в яку повинні входити приміщення виду «Аудиторія». Оновлювати представлення «Аудиторії».*/
-CREATE TRIGGER TR_ROOM_DEL ON rooms
-INSTEAD OF DELETE 
-AS
-BEGIN
- SET NOCOUNT ON;
- DELETE r
- FROM rooms r
- INNER JOIN deleted AS d
- ON r.id=d.id 
- INNER JOIN subdivisions s
- ON r.subdivision_id=s.id
- WHERE s.title=NOT NULL;
- END;
+CREATE OR REPLACE FUNCTION trig_room_delete_checker() RETURNS trigger AS
+$$
+	BEGIN
+		IF EXISTS (SELECT id FROM subdivisions WHERE id = OLD.id) THEN
+            RAISE EXCEPTION 'cannot delete room, that has subdivision';
+        END IF;
+	END;
+$$
+LANGUAGE plpgsql VOLATILE;
 
+CREATE TRIGGER delete_checker
+BEFORE DELETE 
+ON rooms
+FOR EACH ROW EXECUTE PROCEDURE trig_room_delete_checker();
+
+DELETE FROM rooms WHERE id=1;
 
 
