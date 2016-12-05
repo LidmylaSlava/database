15b54f796f84ea7aa73c6bc5b713c0125df8f639
Liudmyla Slava <liudmyla.slava@gmail.com>
2016-12-05 19:11:42 +0200

    Update lab7.sql

--- lab7.sql
+++ lab7.sql
@@ -9,3 +9,20 @@ DECLARE
   END;
   $$ language plpgsql;
   SELECT get_rooms_count_by_subdivision ('deanery');
+ 
+ --доп задание
+  create or replace function room_inserter(rm char(4), beds integer, gen gender, cl class, price integer)
+returns void as
+$$
+declare
+is_room_in rooms%rowtype;
+begin
+select R.* into is_room_in from rooms R where R.room = rm;
+if is_room_in is NULL then
+insert into rooms (room, beds, type, class, price_per_night) values (rm, beds, gen, cl, price);
+else
+raise exception 'Already there!';
+end if;
+end;
+$$
+language plpgsql;
