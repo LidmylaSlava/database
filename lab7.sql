d7fd1dbe51b26b67d06bede90de7a28ce50f00ec
Liudmyla Slava <liudmyla.slava@gmail.com>
2016-12-13 04:51:31 +0200

    Update lab7.sql

--- lab7.sql
+++ lab7.sql
@@ -10,19 +10,30 @@ DECLARE
   $$ language plpgsql;
   SELECT get_rooms_count_by_subdivision ('deanery');
  
- --доп задание
-  create or replace function room_inserter(rm char(4), beds integer, gen gender, cl class, price integer)
+ --доп задание: функция на добавление новой комнаты, проверяя что комнаты с таким номером не существует
+create or replace function room_inserter(id integer, sub_id integer, rm_type_id integer, 
+                                           rm_number integer, sq integer, cap integer)
 returns void as
 $$
 declare
 is_room_in rooms%rowtype;
 begin
-select R.* into is_room_in from rooms R where R.room = rm;
+select R.* into is_room_in from rooms R where R.room_number = rm_number;
 if is_room_in is NULL then
-insert into rooms (room, beds, type, class, price_per_night) values (rm, beds, gen, cl, price);
+insert into rooms (id, subdivision_id, room_type_id, room_number, square, capacity) values (id, sub_id, rm_type_id, rm_number, 
+                                                                                           sq, cap);
 else
 raise exception 'Already there!';
 end if;
 end;
 $$
 language plpgsql;
+select * from room_inserter(25, 13, 2, 156, 20, 15);
+
+/*rooms 
+  id serial NOT NULL,
+  subdivision_id integer,
+  room_type_id integer,
+  room_number integer NOT NULL DEFAULT 1,
+  square integer,
+  capacity integer,
